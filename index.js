47daf0f7539267838558f891729028507d115e65
micheledidonna01 <micheledidonna337@gmail.com>
2025-03-26 10:21:25 +0100

    feat: track bonus solutions

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello Boolean');
+console.log(process.argv);
+console.log(process.argv[2]);
+
+const message = process.argv[2] ?? "Hello World"; //undefined null o stringa vuota
+//const message = process.argv[2] || "Hello World"; se è nullo o undefined ma non se è una stringa vuota
+//const message = process.argv[2] ? process.argv[2] : "Hello World"; 
+
+console.log(message);
+
